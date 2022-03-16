b61775f1a1306d1e7ddccb1f4f025eee7b38bc0f
JayrahBautista <201980058@psu.palawan.edu.ph>
2022-03-16 18:06:05 +0800

    2nd commit

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'dumbpotato');
-	define('DB_NAME', 'log_a');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479357');
+	define('DB_PASS', 'cjCyEqFdLU');
+	define('DB_NAME', 'sql6479357');
